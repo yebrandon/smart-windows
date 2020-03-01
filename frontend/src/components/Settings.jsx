5ea087c269e65e1472f8694f3b3bae21138048f2
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2020-02-29 22:31:28 -0500

    fix POST requests

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,117 +1,70 @@
-import React from "react";
-import axios from "axios";
-import Form from "./Form.jsx";
-import "./Settings.css";
+import React from "react"
+import axios from "axios"
+import "./Settings.css"
 class Settings extends React.Component 
 {
 	constructor(props) 
 	{
 		super(props);
-		this.state = {
-			country: '',
-			city:'',
+		this.state = 
+		{
+			country :'',
+			city : '',
 			pref_temp: 0.0,
 			open_time: [],
 			close_time: [],
-			mode:'',
-		  };
-		this.handleLocationChange = this.handleLocationChange.bind(this);
-		this.handleTempChange = this.handleTempChange.bind(this);
-		this.sendLocation = this.sendLocation.bind(this);
-		this.sendTemp = this.sendTemp.bind(this);
+			mode:'auto',
+		};
 	}
 
-	async sendLocation()
-    { 
-		var location = { "country" : this.country, "city" : this.city}
-        const response = await axios.post('http://localhost:5000/data/settings', location)
+	changeHandler = (e) =>
+	{
+		this.setState({[e.target.name]: e.target.value})
 	}
 
-	async sendTemp()
+	handleSubmit = (e) =>
     {
-        const response = await axios.post('http://localhost:5000/data/settings', this.pref_temp)
-	}
-	async sendOpen()
-    {
-        const response = await axios.post('http://localhost:5000/data/settings', this.open_time)
-	}
-	async sendClose()
-    {
-        const response = await axios.post('http://localhost:5000/data/settings', this.pref_temp)
-	}
-	async sendMode()
-    {
-        const response = await axios.post('http://localhost:5000/data/settings', this.mode)
-	}
-  
-	handleCountryChange (evt) 
-	{
-		this.setState({ country: evt.target.value });
-	}
-	handleCityChange (evt) 
-	{
-		this.setState({ city: evt.target.value });
-	}
-	  
-	handleTempChange (evt) 
-	{
-		this.setState({ pref_temp: evt.target.value });
-	}
-
-	handleOpenChange (evt) 
-	{
-		this.setState({ open_time: evt.target.value });
-	}
-
-	handleCloseChange (evt) 
-	{
-		this.setState({ close_temp: evt.target.value });
-	}
-
-	handleSubmit(event) 
-    {
-		this.sendMode();
-		this.sendLocation();
-		this.sendTemp();
-		this.sendClose();
-		this.sendOpen();
+		e.preventDefault()
+		console.log(this.state)
+		axios
+			.post('https://jsonplaceholder.typicode.com/posts', this.state)
+			.then(response => {
+				console.log(response)
+			})
+			.catch(error =>{
+				console.log(error)
+			})
     }
 
 	render() 
 	{
+		
+		const { city, country, pref_temp, mode } = this.state
 		return (
 			<>
 				<h1>Settings</h1>
 				<form onSubmit={this.handleSubmit}>
       
-				<label for="mode">Choose a mode:</label>
-				<select id="mode" name="mode">
-					<option value="auto">Automatic</option>
-					<option value="saab">Manual</option>
-				</select>
+				<label>Preferred Temperature (Celcius): </label>
+				<input type="number" name="pref_temp" value = {parseFloat(pref_temp)} onChange={this.changeHandler} />
 				<br></br>
 
 				<label>Country: </label>
-				<input type="text" name="country" onChange={this.handleCountryChange} />
+				<input type="text" name="country" onChange={this.changeHandler} />
 				<br></br>
 				
 				<label>City: </label>
-				<input type="text" name="city" onChange={this.handleCityChange} />
+				<input type="text" name="city" onChange={this.changeHandler} />
 				<br></br>
 
-				<label>Preferred Temperature (Celcius): </label>
-				<input type="number" name="pref_temp" onChange={this.handleTempChange} />
+				<label for="mode">Choose a mode:</label>
+				<select id="mode" name="mode" onChange = {this.changeHandler}>
+					<option value="auto">Automatic</option>
+					<option value="man">Manual</option>
+				</select>
 				<br></br>
 
-				{/*<label>Open Time: </label>
-				<input type="time" name="open_temp" onChange={this.handleOpenChange} />
-				<br></br>*/}
-
-				{/*<label>Close Time: </label>
-				<input type="time" name="open_temp" onChange={this.handleCloseChange} />
-				<br></br>*/}
-
-				<input type="submit" value="Submit" />
+				<button type = "submit">Submit</button>
 				</form>
 
 			</>
